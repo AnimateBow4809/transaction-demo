3ec28818d0deec05b258e4068b0d500794fdcfc2
AnimateBow4809 <alidashtbozorg709@gmail.com>
2024-08-07 22:57:19 +0330

    initial commit

---
+++ src/test/java/animate/bow/transactiondemo/TransactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package animate.bow.transactiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
